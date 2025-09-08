3591908114b91f96f4d95345105fa4e70c4a71de
WiamRaghe <wiamraghe@gmail.com>
2025-09-08 22:17:10 +0300

    wip: save local changes before rebase

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -1,17 +1,50 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
     public function index() {
-    return view('shop.welcome');
+        return view('shop.home');
+    }
+
+    public function services() {
+        return view('shop.category');
+    }
+
+    public function cart() {
+        return view('shop.cart');
+    }
+
+    public function about() {
+        return view('shop.about');
+    }
+
+    public function contact() {
+        return view('shop.contact');
+    }
+    
+    public function product($id) {
+        // تمرير id للصفحة لو تحتاج
+        return view('shop.product-details', compact('id'));
+    }
+     public function products() {
+        return view('shop.products');
+    }
+    public function checkout() {
+        return view('shop.checkout');
+    }
+
+    public function submit(Request $request) {
+        // هنا منطق إرسال رسالة contact
+    }
 }
 
-}
+
 
 // namespace App\Http\Controllers;
 
