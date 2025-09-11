d3ff4800991fdf3dbc88ffb0ed2bcc3d11d4edd7
WiamRaghe <wiamraghe@gmail.com>
2025-09-12 00:01:51 +0300

    feat: Apply middleware for route protection

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware(['auth', 'can:access-admin-panel'])->prefix('admin')->group(function () {
+    // ... existing routes ...
+});
 
 require __DIR__.'/auth.php';
